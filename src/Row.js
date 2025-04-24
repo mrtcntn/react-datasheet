9623ea80d8c9e997046cfa3fb8cab4684ed950a0
Mert Canatan <mert.cntn@gmail.com>
2025-04-24 18:31:04 +0300

    refactor(Row): Convert to functional component
    Converts the simple Row class component to a functional component using React.memo.

--- src/Row.js
+++ src/Row.js
@@ -1,17 +1,19 @@
-import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import React, { memo } from 'react';
 
 import CellShape from './CellShape';
 
-class Row extends PureComponent {
-  render() {
-    return <tr>{this.props.children}</tr>;
-  }
-}
+// Convert to simple functional component, use memo
+const Row = memo(({ children }) => {
+  return <tr>{children}</tr>;
+});
 
 Row.propTypes = {
   row: PropTypes.number.isRequired,
   cells: PropTypes.arrayOf(PropTypes.shape(CellShape)).isRequired,
+  children: PropTypes.node, // Add prop type for children
 };
+
+Row.displayName = 'Row'; // Add display name
 
 export default Row;
--- types/Row.d.ts
+++ types/Row.d.ts
@@ -1,28 +1,3 @@
 export default Row;
-declare class Row extends React.PureComponent<any, any, any> {
-    constructor(props: any);
-    constructor(props: any, context: any);
-    render(): React.JSX.Element;
-}
-declare namespace Row {
-    namespace propTypes {
-        let row: PropTypes.Validator<number>;
-        let cells: PropTypes.Validator<(PropTypes.InferProps<{
-            readOnly: PropTypes.Requireable<boolean>;
-            key: PropTypes.Requireable<string>;
-            className: PropTypes.Requireable<string>;
-            component: PropTypes.Requireable<NonNullable<PropTypes.ReactElementLike | ((...args: any[]) => any) | null | undefined>>;
-            forceComponent: PropTypes.Requireable<boolean>;
-            disableEvents: PropTypes.Requireable<boolean>;
-            disableUpdatedFlag: PropTypes.Requireable<boolean>;
-            colSpan: PropTypes.Requireable<number>;
-            rowSpan: PropTypes.Requireable<number>;
-            width: PropTypes.Requireable<NonNullable<string | number | null | undefined>>;
-            overflow: PropTypes.Requireable<string>;
-            dataEditor: PropTypes.Requireable<(...args: any[]) => any>;
-            valueViewer: PropTypes.Requireable<(...args: any[]) => any>;
-        }> | null | undefined)[]>;
-    }
-}
+declare const Row: React.NamedExoticComponent<object>;
 import React from 'react';
-import PropTypes from 'prop-types';
