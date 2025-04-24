37e0173ec9d11b2c2f9849c6ecd784569c7ef128
Mert Canatan <mert.cntn@gmail.com>
2025-04-24 18:30:55 +0300

    refactor(Sheet): Convert to functional component
    Converts the simple Sheet class component to a functional component using React.memo.

--- src/Sheet.js
+++ src/Sheet.js
@@ -1,19 +1,21 @@
-import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import React, { memo } from 'react';
 
-class Sheet extends PureComponent {
-  render() {
-    return (
-      <table className={this.props.className}>
-        <tbody>{this.props.children}</tbody>
-      </table>
-    );
-  }
-}
+// Convert to simple functional component, use memo
+const Sheet = memo(({ className, children }) => {
+  return (
+    <table className={className}>
+      <tbody>{children}</tbody>
+    </table>
+  );
+});
 
 Sheet.propTypes = {
   className: PropTypes.string,
   data: PropTypes.array.isRequired,
+  children: PropTypes.node, // Add prop type for children
 };
+
+Sheet.displayName = 'Sheet'; // Add display name
 
 export default Sheet;
--- types/Sheet.d.ts
+++ types/Sheet.d.ts
@@ -1,14 +1,3 @@
 export default Sheet;
-declare class Sheet extends React.PureComponent<any, any, any> {
-    constructor(props: any);
-    constructor(props: any, context: any);
-    render(): React.JSX.Element;
-}
-declare namespace Sheet {
-    namespace propTypes {
-        let className: PropTypes.Requireable<string>;
-        let data: PropTypes.Validator<any[]>;
-    }
-}
+declare const Sheet: React.NamedExoticComponent<object>;
 import React from 'react';
-import PropTypes from 'prop-types';
